331283d1f71ee73a3755642fa213d76a3de07440
Nguyen Huu Quan <huuquan.nguyen.qt@gmail.com>
2023-09-15 14:05:22 +0700

    finish search function

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # hubtjobs reactive
 
-**demo create hiring post**
+**demo search post**
 
 
-https://github.com/huuquannguyen/hubtjobsreactive/assets/80301438/93de4f61-219d-444e-a222-d354c73704d0
+https://github.com/huuquannguyen/hubtjobsreactive/assets/80301438/2b39078f-8684-4695-895c-718509242f98
--- src/main/java/com/example/hubtjobsreactive/repository/HiringPostRepository.java
+++ src/main/java/com/example/hubtjobsreactive/repository/HiringPostRepository.java
@@ -1,7 +1,16 @@
 package com.example.hubtjobsreactive.repository;
 
 import com.example.hubtjobsreactive.entity.HiringPost;
+import org.springframework.data.r2dbc.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Flux;
 
 public interface HiringPostRepository extends ReactiveCrudRepository<HiringPost, Long> {
+    @Query("SELECT * from hiring_post where lower(title) like lower(concat('%', :keyword, '%')) " +
+            "or lower(category) like lower(concat('%', :keyword, '%')) " +
+            "or lower(tag1) = lower(:keyword) " +
+            "or lower(tag2) = lower(:keyword) " +
+            "or lower(tag3) = lower(:keyword)")
+    Flux<HiringPost> findByKeyword(@Param("keyword") String keyword);
 }
